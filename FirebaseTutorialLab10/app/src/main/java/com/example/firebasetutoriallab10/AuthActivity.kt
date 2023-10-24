a5ba5b2ac84735b8ae33f418666a0afb3133c8fd
Joaquin Campos <cam22155@uvg.edu.gt>
2023-10-24 09:12:09 -0600

    autenticacion de firebase
    no me deja cambiar el xml por los commits del git

--- FirebaseTutorialLab10/app/src/main/java/com/example/firebasetutoriallab10/AuthActivity.kt
+++ FirebaseTutorialLab10/app/src/main/java/com/example/firebasetutoriallab10/AuthActivity.kt
@@ -3,10 +3,10 @@ package com.example.firebasetutoriallab10
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
 
         val analytics = FirebaseAnalytics.getInstance(this)
         val bundle = Bundel()
